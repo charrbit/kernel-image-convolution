5e2dbc93e0fb920d9a10ec08bb26ca2b2d01eb0d
charrbit <40149235+charrbit@users.noreply.github.com>
2023-10-10 17:34:40 -0500

    Docs: Adding descriptive comments

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,5 +1,6 @@
 #include "Matrix.h"
 
+// Constructor definition for square and non-square user-populated Matrix
 Matrix::Matrix(int numRows, int numCols) : n_rows(numRows), n_cols(numCols) {
     matrix_data = new double*[numRows];
     for (int row = 0; row < numRows; row++) {
@@ -11,6 +12,7 @@ Matrix::Matrix(int numRows, int numCols) : n_rows(numRows), n_cols(numCols) {
     }    
 }
 
+// Constructor definition for square and non-square file-populated Matrix
 Matrix::Matrix(int numRows, int numCols, std::istream& inputMatrix) : n_rows(numRows), n_cols(numCols) {
     matrix_data = new double*[numRows];
     for (int row = 0; row < numRows; row++) {
