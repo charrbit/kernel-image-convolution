a8d72a8c5db8389dbe190635c94e957a1c7fe979
charrbit <40149235+charrbit@users.noreply.github.com>
2023-10-11 16:50:03 -0500

    feat: adding configuration file for reading/writing useful image information

--- imageToPixelRGB.py
+++ imageToPixelRGB.py
@@ -19,3 +19,10 @@ blueBand = np.array(image.getdata(2))
 np.savetxt("imageRed.txt", redBand, fmt='%i')
 np.savetxt("imageGreen.txt", greenBand, fmt='%i')
 np.savetxt("imageBlue.txt", blueBand, fmt='%i')
+
+# Write the size and mode of the original image to a configuration file
+configFilename = "imageConfig.txt"
+with open(configFilename, "w") as outFile:
+    outFile.write(f"{imageMode}\n")
+    outFile.write(f"{imageSize[0]}\n") # Width
+    outFile.write(f"{imageSize[1]}\n") # Height
