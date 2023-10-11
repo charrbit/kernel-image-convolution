17c7085cc2b4a910e4adef50384c830195556a3c
charrbit <40149235+charrbit@users.noreply.github.com>
2023-10-11 14:30:58 -0500

    feat: update to sequential convolve allowing for RGB band images

--- main.cpp
+++ main.cpp
@@ -1,14 +1,65 @@
 #include <fstream>
-#include "Matrix.h"
-#include "convolve.cpp"
+
+#include "convolveSequential.cpp"
 
 int main() {
 
-    /* Generate Kernel Matrix */
-    std::ifstream dataSource;
-    dataSource.open("kernels/boxcar.txt");
-    Matrix kernel = Matrix(dataSource, 3);
-    dataSource.close();
+    int imageWidth = 1024;
+    int imageHeight = 1024;
+
+    // Generate the new matrices to store the convolved pixel values
+    Matrix convolvedRed = Matrix(imageHeight, imageWidth);
+    Matrix convolvedGreen = Matrix(imageHeight, imageWidth);
+    Matrix convolvedBlue = Matrix(imageHeight, imageWidth);
+
+    // Generate the kernel Matrix
+    std::ifstream kernelStream;
+    kernelStream.open("kernels/boxcar3.txt");
+    Matrix kernel = Matrix(3, kernelStream);
+    kernelStream.close();
+
+    // Read the RGB pixel values of the original image into a Matrix
+    std::ifstream redPixelInStream;
+    redPixelInStream.open("imageRed.txt");
+    Matrix imageRed = Matrix(imageHeight, imageWidth, redPixelInStream);
+    redPixelInStream.close();
+
+    std::ifstream greenPixelInStream;
+    greenPixelInStream.open("imageGreen.txt");
+    Matrix imageGreen = Matrix(imageHeight, imageWidth, greenPixelInStream);
+    greenPixelInStream.close();
+
+    std::ifstream bluePixelInStream;
+    bluePixelInStream.open("imageBlue.txt");
+    Matrix imageBlue = Matrix(imageHeight, imageWidth, bluePixelInStream);
+    bluePixelInStream.close();
+
+    // Sequentially convolve each RGB Matrix seperately
+    sequentialConvolve(0, imageWidth-1, 0, imageHeight-1, convolvedRed, imageRed, kernel);
+    sequentialConvolve(0, imageWidth-1, 0, imageHeight-1, convolvedGreen, imageGreen, kernel);
+    sequentialConvolve(0, imageWidth-1, 0, imageHeight-1, convolvedBlue, imageBlue, kernel);
+
+    // Write the convolved RGB matrices to their respective files
+    std::ofstream redPixelOutStream;
+    redPixelOutStream.open("imageRedConvolved.txt");
+    if (redPixelOutStream.is_open()) {
+        redPixelOutStream << convolvedRed;
+    }
+    redPixelOutStream.close();
+
+    std::ofstream greenPixelOutStream;
+    greenPixelOutStream.open("imageGreenConvolved.txt");
+    if (greenPixelOutStream.is_open()) {
+        greenPixelOutStream << convolvedGreen;
+    }
+    greenPixelOutStream.close();
+
+    std::ofstream bluePixelOutStream;
+    bluePixelOutStream.open("imageBlueConvolved.txt");
+    if (bluePixelOutStream.is_open()) {
+        bluePixelOutStream << convolvedBlue;
+    }
+    bluePixelOutStream.close();
 
 
     /* Generate Parallel Convolved Image */
@@ -20,29 +71,6 @@ int main() {
     // Matrix newPimage = Matrix(n);
     // int stopSize = 1;
     // parallelConvolve(0, 0, newPimage.getRow(), newPimage.getCol(), newPimage, image, kernel, stopSize, stopSize);
-
-    // -----------------------------------------------------
-
-
-    /* Generate Sequential Convolved Image */
-
-    /* Matrix newImage = Matrix(n);
-    
-    sequentialConvolve(0, newImage.getRow(), 0 , newImage.getCol(), newImage, image, kernel);
-
-
-    // -----------------------------------------------------
-
-
-    /* Write NewImage to file */
-
-    /* std::ofstream saveFile;
-    saveFile.open("convolvedData.txt");
-    saveFile << newPImage;
-    saveFile.close(); */
-
-    // -----------------------------------------------------
-
 
     return 0;
 }
