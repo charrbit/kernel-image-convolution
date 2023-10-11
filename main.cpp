8cf8147671fe91b8e2f4b0439be5efd2e7ced9a6
charrbit <40149235+charrbit@users.noreply.github.com>
2023-10-11 14:04:15 -0500

    feat: removing random matrix generation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <fstream>
-#include <random>
 #include "Matrix.h"
 #include "convolve.cpp"
 
@@ -10,29 +9,6 @@ int main() {
     dataSource.open("kernels/boxcar.txt");
     Matrix kernel = Matrix(dataSource, 3);
     dataSource.close();
-
-    // -----------------------------------------------------
-
-
-    /* Generate Image Matrix */
-   
-    /* Matrix image = Matrix(n);
-
-    std::random_device seed;
-    std::mt19937 rng(seed());
-    std::uniform_int_distribution<std::mt19937::result_type> range(0,65535);
-
-    for (int i = 0; i < image.getRow(); i++) {
-        for (int j = 0; j < image.getCol(); j++) {
-            image.set(i,j, range(rng));
-        }
-    }
-
-    std::ofstream outFile;
-    outFile.open("Image_8192.txt");
-    outFile << image; */
- 
-    // -----------------------------------------------------
 
 
     /* Generate Parallel Convolved Image */
